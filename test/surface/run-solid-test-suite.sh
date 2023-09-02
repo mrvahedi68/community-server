a359539d3facc88fa729a91ef51bd94fd6deea1d
MohammadReza vahedi <mr.vahedi68@gmail.com>
2023-09-02 20:04:56 +0000

    fixing for WAC
    Signed-off-by: MohammadReza vahedi <mr.vahedi68@gmail.com>

--- test/surface/run-solid-test-suite.sh
+++ test/surface/run-solid-test-suite.sh
@@ -4,7 +4,7 @@ set -e
 export SKIP_CONC=1
 
 function setup {
-  npm ci
+  #npm ci
   npm start -- --detectOpenHandles &> '/dev/null' &
   git clone https://github.com/solid-contrib/solid-crud-tests.git
   cd solid-crud-tests
@@ -24,7 +24,12 @@ function setup {
 }
 
 function runThirdParty {
-  npm start -- --port 3001 --detectOpenHandles &
+  git clone https://github.com/solid/node-solid-server
+  cd node-solid-server
+  docker build -t node-solid-server .
+
+  docker run -d -p 8443:8443 --name server node-solid-server
+  cd ..
 }
 
 function waitForCss {
@@ -35,10 +40,21 @@ function waitForCss {
   done
 }
 
+function waitForNss {
+  until curl -kI https://localhost 2> /dev/null
+  do
+    echo Waiting for NSS to start on port 443, this can take up to a minute ...
+    docker logs server
+    sleep 1
+  done
+}
+
 function teardown {
   kill $(lsof -t -i :3000)
-  kill $(lsof -t -i :3001)
-  rm -rf solid-crud-tests web-access-control-tests
+  rm -rf solid-crud-tests web-access-control-tests test-suite
+  docker stop `docker ps --filter network=testnet -q`
+  docker rm `docker ps --filter network=testnet -qa`
+  docker network remove testnet
 }
 
 function runTests {
@@ -51,9 +67,9 @@ function runTests {
 teardown || true
 setup
 waitForCss 3000
-runTests solid-crud-tests
-# runThirdParty
-# waitForCss 3001
-# runTests web-access-control-tests
+#runTests solid-crud-tests
+#runThirdParty
+#waitForNss
+runTests web-access-control-tests
 teardown
 
